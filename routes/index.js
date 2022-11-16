f5b52eae10460bec411709518d4d932283a18a72
Youness <y.ouzzedi@hotmail.com>
2022-11-16 16:57:55 +0100

    change route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const fetch = require('node-fetch');
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
+router.get('/', (req, res) => {
+  res.render('index.html')
+})
+
 router.get('/movies', (req, res) => {
         fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
  .then(response => response.json())
